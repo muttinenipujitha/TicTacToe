8eec65c07fdf4af5970a361cc7666ae7e0237cf0
pujitha <20b01a12b7@svecw.edu.in>
2026-03-29 18:36:50 +0530

    Implement server-authoritative game logic in Nakama

--- nakama/modules/main.js
+++ nakama/modules/main.js
@@ -8,7 +8,7 @@ function checkWinner(board) {
     [0,1,2],[3,4,5],[6,7,8],
     [0,3,6],[1,4,7],[2,5,8],
     [0,4,8],[2,4,6]
-  ];
+  ];   
   for (var i = 0; i < wins.length; i++) {
     var a = wins[i][0], b = wins[i][1], c = wins[i][2];
     if (board[a] && board[a] === board[b] && board[a] === board[c]) return board[a];
